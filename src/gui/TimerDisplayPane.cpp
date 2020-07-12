7b48f24ef90454185721e71b2d531384ec988add
AyanamiRei0 <danieladlem4@gmail.com>
2020-07-12 06:56:22 +0100

    Update TimerDisplayPane.cpp

--- src/gui/TimerDisplayPane.cpp
+++ src/gui/TimerDisplayPane.cpp
@@ -47,7 +47,7 @@ namespace gui {
             {
                 rootLayout->addWidget(currentStage);
                 rootLayout->setAlignment(currentStage, Qt::AlignLeft);
-                const int font = QFontDatabase::addApplicationFont(":/fonts/RobotoMono-Regular.ttf");
+                const int font = QFontDatabase::addApplicationFont(":/fonts/Poketch.ttf");
                 const QString family = QFontDatabase::applicationFontFamilies(font)[0];
                 currentStage->setObjectName("currentStageLbl");
                 currentStage->setFont(QFont(family, 36));
